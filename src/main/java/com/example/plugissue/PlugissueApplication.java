11f6a2d8561f351ddf7aa5bf2da3d6260223a309
topsilverr <wltkddms9909@naver.com>
2023-08-09 16:15:45 +0900

    initial commit

---
+++ src/main/java/com/example/plugissue/PlugissueApplication.java
@@ -0,0 +1,13 @@
+package com.example.plugissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlugissueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlugissueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/plugissue/PlugissueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plugissue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlugissueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
