ec6da1ba85620d3ca6c453c898f0cd3ff126c9a2
topsilverr <wltkddms9909@naver.com>
2023-08-16 00:43:54 +0900

    [feat] 주어진 id 에 해당하는 충전소 상세 정보 조회 구현

--- src/main/java/com/example/plugissue/station/repository/StationRepository.java
+++ src/main/java/com/example/plugissue/station/repository/StationRepository.java
@@ -4,19 +4,23 @@ import com.example.plugissue.station.entity.Station;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
-import java.util.Objects;
 
 public interface StationRepository extends JpaRepository<Station,Long> {
 
     @Query("SELECT s, st FROM Station s " +
-            "LEFT JOIN Status st ON s.id = st.station.id " +
+            "LEFT JOIN Status st ON s.id = st.station.sId " +
             "WHERE 6371 * acos(" +
             "cos(radians(:lat)) * cos(radians(s.latitude)) * " +
             "cos(radians(s.longitude) - radians(:lng)) + " +
             "sin(radians(:lat)) * sin(radians(s.latitude))" +
             ") <= :range")
     List<Object[]> findStationsByLoc(@Param("lat") Double lat, @Param("lng") Double lng, @Param("range") Integer range);
+
+    @Query("SELECT s, st FROM Station s " +
+            "LEFT JOIN Status st ON :sId = st.station.sId ")
+    Object[] findStationById(@PathVariable("stationId") Long sId);
 
 }
