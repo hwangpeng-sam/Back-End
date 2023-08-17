e94fa408a9c4ec3f94cbf1acea6f72a8d8f56c21
topsilverr <wltkddms9909@naver.com>
2023-08-17 09:04:25 +0900

    [chore] to checkout

---
+++ src/test/java/com/example/plugissue/station/repository/stationRepositoryTest.java
@@ -0,0 +1,37 @@
+//package com.example.plugissue.station.repository;
+//
+//import com.example.plugissue.station.entity.Station;
+//import com.example.plugissue.station.service.StationService;
+//import com.example.plugissue.status.repository.StatusRepository;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@SpringBootTest
+//@Transactional
+//public class stationRepositoryTest {
+//
+//    @Autowired
+//    StationRepository stationRepository;
+//    @Autowired
+//    StatusRepository statusRepository;
+//
+//    @Test
+//    void getStationList(){
+//        //given
+//        Station station = new Station(1L, 127, 38, )
+//        //when
+//
+//        //then
+//    }
+//
+//    @Test
+//    void getStationDetail(){
+//        //given
+//
+//        //when
+//
+//        //then
+//    }
+//}
