2f9a6c2f59c8b2923da780bd7bb7183d23c14c79
topsilverr <wltkddms9909@naver.com>
2023-08-17 18:07:13 +0900

    [fix] getStationDetails 쿼리 수정

--- src/main/java/com/example/plugissue/station/controller/StationController.java
+++ src/main/java/com/example/plugissue/station/controller/StationController.java
@@ -28,10 +28,10 @@ public class StationController {
 
     @ResponseBody
     @GetMapping("/station/{sId}")
-    public ResponseEntity<StationStatusDto>getStationDetails(
+    public ResponseEntity<List<StationStatusDto>>getStationDetails(
             @PathVariable("sId") Long stationId
     ){
-        StationStatusDto stationStatusDto = stationService.findById(stationId);
+        List<StationStatusDto> stationStatusDto = stationService.findById(stationId);
         return ResponseEntity.ok(stationStatusDto);
     }
 }
