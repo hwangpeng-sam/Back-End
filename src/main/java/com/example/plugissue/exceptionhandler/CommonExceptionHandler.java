afa67ceba07d78275397561217f4fafbedf9a35f
topsilverr <wltkddms9909@naver.com>
2023-08-22 09:03:27 +0900

    [fix] 예외처리 수정

--- src/main/java/com/example/plugissue/exceptionhandler/CommonExceptionHandler.java
+++ src/main/java/com/example/plugissue/exceptionhandler/CommonExceptionHandler.java
@@ -3,6 +3,7 @@ package com.example.plugissue.exceptionhandler;
 import com.example.plugissue.exception.notfound.config.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Map;
@@ -10,6 +11,12 @@ import java.util.Map;
 @RestControllerAdvice
 public class CommonExceptionHandler {
 
+    /**
+     * ResourceNotFoundException 타입 예외처리 ExceptionHandler
+     * @param e ResourceNotFoundException
+     * @return ErrorResponse
+     */
+    @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponse> ResourceNotFoundExceptionHandler(ResourceNotFoundException e){
         ErrorResponse errorResponse = ErrorResponse.builder()
                 .code(e.getErrorCode().getCode())
