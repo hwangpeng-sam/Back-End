f5758facd893968190736956115ef84f69c64175
topsilverr <wltkddms9909@naver.com>
2023-08-28 14:09:57 +0900

    [feat] 4시간 통계값 추가

--- src/main/java/com/example/plugissue/station/controller/dto/StationStatusAvgDto.java
+++ src/main/java/com/example/plugissue/station/controller/dto/StationStatusAvgDto.java
@@ -13,17 +13,18 @@ public class StationStatusAvgDto {
     private Double charAvg40;
     private Double charAvg60;
     private Double charAvg120;
-
+    private Double charAvg4;
 
     public StationStatusAvgDto(){
 
     }
-    public StationStatusAvgDto(Station station, Status status, Double charAvg20, Double charAvg40, Double charAvg60, Double charAvg120){
+    public StationStatusAvgDto(Station station, Status status, Double charAvg20, Double charAvg40, Double charAvg60, Double charAvg120, Double charAvg4){
         this.station = station;
         this.status = status;
         this.charAvg20 = charAvg20;
         this.charAvg40 = charAvg40;
         this.charAvg60 = charAvg60;
         this.charAvg120 = charAvg120;
+        this.charAvg4 = charAvg4;
     }
 }
